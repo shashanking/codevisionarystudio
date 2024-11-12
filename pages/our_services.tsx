b1964644a872a0bfee86f85fc1f5460ee423e08c
Shohan Ghosh <shohanghosh3@gmail.com>
2024-11-12 18:13:24 +0530

    LandingSection Done of OurServices

---
+++ pages/our_services.tsx
@@ -0,0 +1,12 @@
+import OurServicesLanding from '@/components/OurServices/OurServicesLanding'
+import React from 'react'
+
+const OurServices = () => {
+  return (
+    <div>
+      <OurServicesLanding />
+    </div>
+  )
+}
+
+export default OurServices
