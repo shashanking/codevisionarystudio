b66942e9efbde22cc0bcc8c4ae54ff05fd946cda
shashank singh <shashank@shashanks-MacBook-Air.local>
2024-12-08 16:11:58 +0530

    google analytics and GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,8 @@ import "lenis/dist/lenis.css";
 import "@/styles/globals.scss";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { GoogleTagManager } from '@next/third-parties/google';
-import { GTM_ID, gtmPageView } from "@/lib/gtm";
+import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google';
+import { GTM_ID, GA_ID, gtmPageView } from "@/lib/gtm";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -25,12 +25,21 @@ export default function App({ Component, pageProps }: AppProps) {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, []);
 
+  // Track page views whenever pageProps changes
   useEffect(() => {
     const props = {
       page_title: pageProps.title || null,
       page_path: window.location.pathname,
     };
     gtmPageView(props);
+    
+    // Track page view in GA
+    if (window.gtag) {
+      window.gtag('event', 'page_view', {
+        page_title: pageProps.title || null,
+        page_path: window.location.pathname,
+      });
+    }
   }, [pageProps]);
 
   return (
@@ -39,6 +48,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
       <ToastContainer />
       <GoogleTagManager gtmId={GTM_ID} />
+      <GoogleAnalytics gaId={GA_ID} />
     </>
   );
 }
